a62778cd15e4300c8ab7eacc3fb0cb61a65ca67d
maite <maite.sanchez@gm2dev.com>
2024-10-17 11:10:43 -0300

    feat(script.js): add multiply elements function

--- script.js
+++ script.js
@@ -3,9 +3,23 @@ async function fetchApi() {
         const result = await fetch(`https://api.mockaroo.com/api/generate.json?key=83b46010&count=1000&fields=[{"name":"id","type":"Row Number"},{"name":"first_name","type":"First Name"},{"name":"email","type":"Email Address"}]`);
         const data = await result.json();
 
+        const totalElements = multiplyElements(data);
     } catch (error) {
         console.error('Error fetching data:', error);
     }
+}
+
+function multiplyElements(data){
+    const requiredElements = 1000000;
+    let totalLength = 0;
+    const multipliedElements = [];
+    
+    while(requiredElements > totalLength){
+        multipliedElements.push(...data);
+        totalLength += data.length;
+    }
+
+    return multipliedElements;
 }
 
 fetchApi();
